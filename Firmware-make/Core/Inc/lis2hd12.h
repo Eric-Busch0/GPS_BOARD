3128cf7a92f46c0581dc78b9b69282c0b88c01b7
Alpacabet <49300674+Eric-Busch0@users.noreply.github.com>
2023-06-25 17:51:19 -0500

    IMU API update

--- Firmware-make/Core/Inc/lis2hd12.h
+++ Firmware-make/Core/Inc/lis2hd12.h
@@ -9,28 +9,57 @@ extern "C"
 {
 #endif
 
-#define LISS2HD12_WHO_AM_I 0x33
+#define LIS2HD12_WHO_AM_I 0x33
+
+#define LIS2HD12_STATUS_Y_AVA (1 << 0)
+#define LIS2HD12_STATUS_Z_AVA (1 << 1)
+#define LIS2HD12_STATUS_XYZ_AVA (1 << 2)
+#define LIS2HD12_STATUS_XYZ_OVR (1 << 3)
+#define LIS2HD12_STATUS_X_OVR (1 << 4)
+#define LIS2HD12_STATUS_Y_OVR (1 << 5)
+#define LIS2HD12_STATUS_Z_OVR (1 << 6)
+#define LIS2HD12_STATUS_XYZ_OVR (1 << 7)
 
     typedef enum
     {
         LIS2HD12_ODR_PWR_DOWN,
-        LIS2HD12_ODR_PWR_1HZ,            // 1Hz All Modes
-        LIS2HD12_ODR_PWR_10HZ,           // 10Hz All Modes
-        LIS2HD12_ODR_PWR_25HZ,           // 25Hz All Modes
-        LIS2HD12_ODR_PWR_50HZ,           // 50Hz All Modes
-        LIS2HD12_ODR_PWR_100HZ,          // 100Hz All Modes
-        LIS2HD12_ODR_PWR_200HZ,          // 200Hz All Modes
-        LIS2HD12_ODR_PWR_400HZ,          // 400Hz All Modes
-        LIS2HD12_ODR_PWR_1_62KHZ,        // 1.62kHz All Modes
+        LIS2HD12_ODR_PWR_1HZ,         // 1Hz All Modes
+        LIS2HD12_ODR_PWR_10HZ,        // 10Hz All Modes
+        LIS2HD12_ODR_PWR_25HZ,        // 25Hz All Modes
+        LIS2HD12_ODR_PWR_50HZ,        // 50Hz All Modes
+        LIS2HD12_ODR_PWR_100HZ,       // 100Hz All Modes
+        LIS2HD12_ODR_PWR_200HZ,       // 200Hz All Modes
+        LIS2HD12_ODR_PWR_400HZ,       // 400Hz All Modes
+        LIS2HD12_ODR_PWR_1_62KHZ,     // 1.62kHz All Modes
         LIS2HD12_ODR_PWR_MULTI = 0x9, // 1.344 kHz HR/Normal or 5.376 kHz Low Power Mode
     } lis2hd12_odr_t;
 
     typedef enum
     {
-        LIS2HD12_NORMAL,
-        LIS2HD12_REF_SIG_FILTERING,
-        LIS2HD12_AUTO_RST_ON_INT = 0x3,
+        LIS2HD12_HPF_NORMAL,
+        LIS2HD12_HPF_REF_SIG_FILTERING,
+        LIS2HD12_HPF_AUTO_RST_ON_INT = 0x3,
     } lis2hd12_hpf_mode_t;
+    typedef enum
+    {
+        LIS2HD12_FS_2G,
+        LIS2HD12_FS_4G,
+        LIS2HD12_FS_8G,
+        LIS2HD12_FS_16G,
+    } lis2hd12_fs_mode_t;
+    typedef enum
+    {
+        LIS2HD12_MODE_HR,
+        LIS2HD12_MODE_NORMAL,
+        LIS2HD12_MODE_LOW_PWR,
+    } lis2hd12_operating_mode_t;
+
+    typedef enum
+    {
+        LI2HD12_ST_DISABLE,
+        LI2HD12_ST_0,
+        LI2HD12_ST_1,
+    } lis2hd12_self_test_t;
 
     typedef struct
     {
@@ -45,56 +74,102 @@ extern "C"
             lis2hd12_hpf_mode_t hpfMode;
             uint8_t hpfCutoff;
             uint8_t fds; // Filtered data selection
-            uint8_t hpClick;
-            uint8_t hpIa2, hpIa1;
+            bool hpClick;
+            bool hpIa2En, hpIa1En;
         } ctrl2;
 
-        struct{
-            uint8_t i1Click;
-            uint8_t i1Ia1, i1Ia2;
-            uint8_t i1Zyxda;
-            uint8_t i1Wtm;
-            uint8_t i1Overrun;
-        }ctrl3;
-
-        struct 
+        struct
         {
-            uint8_t blockDataUpdate;
+            bool i1Click;
+            bool i1Ia1, i1Ia2;
+            bool i1Zyxda;
+            bool i1Wtm;
+            bool i1Overrun;
+        } ctrl3;
+
+        struct
+        {
+            bool blockDataUpdate;
             uint8_t bigOrLittleEndian;
-            uint8_t operatingMode;
-            uint8_t selfTestEnable;
+            lis2hd12_fs_mode_t fsMode;
+            lis2hd12_operating_mode_t operatingMode;
+            lis2hd12_self_test_t selfTestMode;
             uint8_t sim;
-        }ctrl4;
-        
-        struct {
+        } ctrl4;
+
+        struct
+        {
             uint8_t boot;
             uint8_t fifoEn;
             uint8_t latchIntReq1;
             uint8_t D4DEn1;
             uint8_t latchIntReq2;
             uint8_t D4DEn2;
-        }ctrl5;
+        } ctrl5;
 
-        struct 
+        struct
         {
-            uint8_t clickInt2;
-            uint8_t int1FnInt2En;
-            uint8_t int2FnInt2En;
-            uint8_t bootOnInt2En;
-            uint8_t activityIntOnInt2En;
+            bool clickInt2;
+            bool int1FnInt2En;
+            bool int2FnInt2En;
+            bool bootOnInt2En;
+            bool activityIntOnInt2En;
             uint8_t int12Polarity;
-        }ctrl6;
-        
-    } lis2dh12_ctrl_t;
+        } ctrl6;
+
+    } lis2hd12_ctrl_t;
     typedef struct
     {
         uint16_t x, y, z;
     } lis2dh12_acc_xyz;
 
-    // Place c code here
-    void lis2hd12_init(void);
     uint8_t lis2hd12_who_am_i(uint8_t *who_am_i);
-    uint8_t lis2hd12_get_ctrl(lis2dh12_ctrl_t *ctrl);
+    uint8_t lis2hd12_get_status_reg(uint8_t *status_reg);
+    uint8_t lis2hd12_get_temp(uint16_t *temp);
+    uint8_t lis2hd12_disconnect_sdo_pullup(bool disconnect);
+    uint8_t lis2hd12_sdo_pullup_is_disconnected(bool *is_disconnected);
+    uint8_t lis2hd12_get_ctrl(lis2hd12_ctrl_t *ctrl);
+    uint8_t lis2hd12_set_ctrl(lis2hd12_ctrl_t *ctrl);
+    uint8_t lis2hd12_enable_temp_sensor(void);
+    uint8_t lis2hd12_disable_temp_sensor(void);
+    uint8_t lis2hd12_temp_sensor_is_enabled(bool *is_enabled);
+    uint8_t lis2hd12_get_interrupt_ref(uint8_t *interrupt_ref);
+    uint8_t lis2hd12_set_interrupt_ref(uint8_t interrupt_ref);
+    uint8_t lis2hd12_get_status(uint8_t *status);
+    uint8_t lis2hd12_get_acc_xyz(lis2dh12_acc_xyz *xyz);
+    uint8_t lis2hd12_get_fifo_ctrl(uint8_t *fifo_ctrl);
+    uint8_t lis2hd12_set_fifo_ctrl(uint8_t fifo_ctrl);
+    uint8_t lis2hd12_get_fifo_src(uint8_t *fifo_src);
+    uint8_t lis2hd12_get_int1_cfg(uint8_t int1_cfg);
+    uint8_t lis2hd12_set_int1_cfg(uint8_t *int1_cfg);
+    uint8_t lis2hd12_get_int1_src(uint8_t *int1_src);
+    uint8_t lis2hd12_get_int1_ths(uint8_t *int1_ths);
+    uint8_t lis2hd12_set_int1_ths(uint8_t int1_ths);
+    uint8_t lis2hd12_get_int1_duration(uint8_t *int1_duration);
+    uint8_t lis2hd12_set_int1_duration(uint8_t int1_duration);
+    uint8_t lis2hd12_get_int2_cfg(uint8_t *int2_cfg);
+    uint8_t lis2hd12_set_int2_cfg(uint8_t int2_cfg);
+    uint8_t lis2hd12_get_int2_src(uint8_t *int2_src);
+    uint8_t lis2hd12_get_int2_ths(uint8_t *int2_ths);
+    uint8_t lis2hd12_set_int2_ths(uint8_t int2_ths);
+    uint8_t lis2hd12_get_int2_duration(uint8_t *int2_duration);
+    uint8_t lis2hd12_set_int2_duration(uint8_t int2_duration);
+    uint8_t lis2hd12_get_click_cfg(uint8_t *click_cfg);
+    uint8_t lis2hd12_set_click_cfg(uint8_t click_cfg);
+    uint8_t lis2hd12_get_click_src(uint8_t *click_src);
+    uint8_t lis2hd12_get_click_ths(uint8_t *click_ths);
+    uint8_t lis2hd12_set_click_ths(uint8_t *click_ths);
+    uint8_t lis2hd12_get_time_limit(uint8_t *time_limit);
+    uint8_t lis2hd12_set_time_limit(uint8_t time_limit);
+    uint8_t lis2hd12_get_time_latency(uint8_t *time_latency);
+    uint8_t lis2hd12_set_time_latency(uint8_t time_latency);
+    uint8_t lis2hd12_get_time_window(uint8_t *time_window);
+    uint8_t lis2hd12_set_time_window(uint8_t time_window);
+    uint8_t lis2hd12_get_act_ths(uint8_t *act_ths);
+    uint8_t lis2hd12_set_act_ths(uint8_t act_ths);
+    uint8_t lis2hd12_get_act_dur(uint8_t *act_dur);
+    uint8_t lis2hd12_set_act_dur(uint8_t act_dur);
+
 
 #ifdef __cplusplus
 }
--- Firmware-make/Core/Src/lis2hd12.c
+++ Firmware-make/Core/Src/lis2hd12.c
@@ -6,9 +6,16 @@
 
 volatile uint8_t write_complete = 0;
 volatile uint8_t read_complete = 0;
+uint8_t wr_buf[8];
+
+lis2hd12_ctrl_t ctrl_reg;
+#define GETBIT(val, bit) ((val & (1 << bit)) >> bit)
+#define GETBITMASK(val, mask, bit) ((val & (mask << bit)) >> bit)
 
 #define LIS2HD12_I2C_ADDR (0x19 << 1)
-#define LIS2HD12_CHECK_NULL(x)  if(x == NULL) return HAL_ERROR
+#define LIS2HD12_CHECK_NULL(x) \
+    if (x == NULL)             \
+    return HAL_ERROR
 
 static inline uint8_t lis2hd12_write(uint8_t *buf, uint32_t len)
 {
@@ -18,12 +25,15 @@ static inline uint8_t lis2hd12_read_reg(uint8_t memaddr, uint8_t *buf, uint32_t 
 {
     return i2c_mem_read(LIS2HD12_I2C_ADDR, memaddr, sizeof(uint8_t), buf, len);
 }
-static inline uint8_t lis2hd12_write_reg(uint8_t reg_addr, uint8_t data)
-{
-    return 0;
-}
 
-void lis2hd12_init(void);
+static inline uint8_t lis2hd12_write_reg(uint8_t memaddr, uint8_t *data, uint32_t len)
+{
+    return i2c_mem_write(LIS2HD12_I2C_ADDR, memaddr, sizeof(uint8_t), data, len);
+}
+static inline uint8_t lis2hd12_write_reg_dma(uint8_t memaddr, uint8_t *data, uint32_t len)
+{
+    return i2c_mem_write_dma(LIS2HD12_I2C_ADDR, memaddr, sizeof(uint8_t), data, len);
+}
 
 uint8_t lis2hd12_who_am_i(uint8_t *who_am_i)
 {
@@ -54,8 +64,7 @@ uint8_t lis2hd12_disconnect_sdo_pullup(bool disconnect)
 
     const uint8_t reg_value = disconnect ? PU_DISCONNECT : PU_CONNECT;
 
-    return lis2hd12_write_reg(CTRL0_REG, reg_value);
-
+    return lis2hd12_write_reg(CTRL0_REG, &reg_value, 1);
 }
 uint8_t lis2hd12_sdo_pullup_is_disconnected(bool *is_disconnected)
 {
@@ -68,41 +77,172 @@ uint8_t lis2hd12_sdo_pullup_is_disconnected(bool *is_disconnected)
     *is_disconnected = (reg_value == PU_DISCONNECT);
 
     return ret;
-
 }
-
-uint8_t lis2hd12_get_ctrl(lis2dh12_ctrl_t *ctrl)
-{
-    return 0;
-}
-
-
-uint8_t lis2hd12_set_ctrl(lis2dh12_ctrl_t *ctrl)
+static void ctrl_regs_to_struct(uint8_t *regs, lis2hd12_ctrl_t *ctrl)
 {
 
+    /*
+        Ctrl 1
+    */
+    ctrl->ctrl1.odr = (lis2hd12_odr_t)((regs[0] & (0x4 << 4)) >> 4);
+    ctrl->ctrl1.lowPwrEn = regs[0] & (1 << 3);
+    ctrl->ctrl1.zAxisEn = regs[0] & (1 << 2);
+    ctrl->ctrl1.yAxisEN = regs[0] & (1 << 1);
+    ctrl->ctrl1.xAxisEn = regs[0] & (1 << 0);
 
-    return 0;
+    /*
+        Ctrl 2
+    */
+    ctrl->ctrl2.hpfMode = (lis2hd12_hpf_mode_t)((regs[1] & (0x2 << 6)) >> 6);
+    ctrl->ctrl2.hpfCutoff = (regs[1] & (0x2 << 4)) >> 4;
+    ctrl->ctrl2.fds = (regs[1] & (1 << 3)) >> 3;
+    ctrl->ctrl2.hpClick = (regs[1] & (1 << 2)) >> 2;
+    ctrl->ctrl2.hpIa2En = (regs[1] & (1 << 1)) >> 1;
+    ctrl->ctrl2.hpIa1En = (regs[1] & (1 << 0));
+
+    /*
+        Ctrl 3
+    */
+    ctrl->ctrl3.i1Click = (regs[2] & (1 << 7)) >> 7;
+    ctrl->ctrl3.i1Ia1 = (regs[2] & (1 << 6)) >> 6;
+    ctrl->ctrl3.i1Ia2 = (regs[2] & (1 << 5)) >> 5;
+    ctrl->ctrl3.i1Zyxda = (regs[2] & (1 << 4)) >> 4;
+    ctrl->ctrl3.i1Wtm = (regs[2] & (1 << 2)) >> 2;
+    ctrl->ctrl3.i1Overrun = (regs[2] & (1 << 1)) >> 1;
+    /*
+        Ctrl 4
+    */
+    ctrl->ctrl4.blockDataUpdate = GETBIT(regs[3], 7);
+    ctrl->ctrl4.bigOrLittleEndian = GETBIT(regs[3], 6);
+    ctrl->ctrl4.fsMode = GETBITMASK(regs[3], 0x2, 4);
+    ctrl->ctrl4.operatingMode = GETBIT(regs[3], 3);
+    ctrl->ctrl4.selfTestMode = GETBITMASK(regs[3], 0x2, 1);
+    ctrl->ctrl4.sim = GETBIT(regs[3], 1);
+
+    /*
+        Ctrl 5
+    */
+    ctrl->ctrl5.boot = GETBIT(regs[4], 7);
+    ctrl->ctrl5.fifoEn = GETBIT(regs[4], 6);
+    ctrl->ctrl5.latchIntReq1 = GETBIT(regs[4], 3);
+    ctrl->ctrl5.D4DEn1 = GETBIT(regs[4], 2);
+    ctrl->ctrl5.latchIntReq2 = GETBIT(regs[4], 1);
+    ctrl->ctrl5.D4DEn2 = GETBIT(regs[4], 0);
+
+    /*
+        Ctrl 6
+    */
+
+    ctrl->ctrl6.clickInt2 = GETBIT(regs[5], 7);
+    ctrl->ctrl6.int1FnInt2En = GETBIT(regs[5], 6);
+    ctrl->ctrl6.int2FnInt2En = GETBIT(regs[5], 5);
+    ctrl->ctrl6.bootOnInt2En = GETBIT(regs[5], 4);
+    ctrl->ctrl6.activityIntOnInt2En = GETBIT(regs[5], 3);
+    ctrl->ctrl6.int12Polarity = GETBIT(regs[5], 1);
+}
+uint8_t lis2hd12_get_ctrl(lis2hd12_ctrl_t *ctrl)
+{
+    assert(ctrl);
+
+    static const uint8_t CTRL_REG1 = 0x20;
+    static const uint8_t NUM_CTRL_REGS = 6u;
+
+    uint8_t regs[6] = {0};
+
+    uint8_t ret = lis2hd12_read_reg(CTRL_REG1, regs, NUM_CTRL_REGS);
+
+    RETCHECK(ret);
+
+    ctrl_regs_to_struct(regs, ctrl);
+
+    return ret;
+}
+static void ctrl_struct_to_regs(uint8_t *regs, lis2hd12_ctrl_t *ctrl)
+{
+    /*
+        Ctrl 1
+    */
+    regs[0] = (ctrl->ctrl1.odr << 4) |
+              (ctrl->ctrl1.lowPwrEn << 3) |
+              (ctrl->ctrl1.zAxisEn << 2) |
+              (ctrl->ctrl1.yAxisEN << 1) |
+              (ctrl->ctrl1.yAxisEN << 1);
+
+    /*
+        Ctrl 2
+    */
+    regs[1] = (ctrl->ctrl2.hpfMode << 6) |
+              (ctrl->ctrl2.hpfCutoff << 4) |
+              (ctrl->ctrl2.fds << 3) |
+              (ctrl->ctrl2.hpClick << 2) |
+              (ctrl->ctrl2.hpIa2En << 1) |
+              (ctrl->ctrl2.hpIa1En << 0);
+    /*
+        Ctrl 3
+    */
+    regs[2] = (ctrl->ctrl3.i1Click << 7) |
+              (ctrl->ctrl3.i1Ia1 << 6) |
+              (ctrl->ctrl3.i1Ia2 << 5) |
+              (ctrl->ctrl3.i1Zyxda << 4) |
+              (ctrl->ctrl3.i1Wtm << 2) |
+              (ctrl->ctrl3.i1Overrun << 1);
+    /*
+        Ctrl 4
+    */
+    regs[3] = (ctrl->ctrl4.blockDataUpdate << 7) |
+              (ctrl->ctrl4.bigOrLittleEndian << 6) |
+              (ctrl->ctrl4.fsMode << 4) |
+              (ctrl->ctrl4.operatingMode << 3) |
+              (ctrl->ctrl4.selfTestMode << 1) |
+              (ctrl->ctrl4.sim << 0);
+    /*
+        Ctrl 5
+    */
+    regs[4] = (ctrl->ctrl5.boot << 7) |
+              (ctrl->ctrl5.fifoEn << 6) |
+              (ctrl->ctrl5.latchIntReq1 << 3) |
+              (ctrl->ctrl5.D4DEn1 << 2) |
+              (ctrl->ctrl5.latchIntReq2 << 1) |
+              (ctrl->ctrl5.D4DEn2 << 0);
+
+    /*
+        Ctrl 6
+    */
+    regs[5] = (ctrl->ctrl6.clickInt2 << 7) |
+              (ctrl->ctrl6.int1FnInt2En << 6) |
+              (ctrl->ctrl6.int2FnInt2En << 5) |
+              (ctrl->ctrl6.bootOnInt2En << 4) |
+              (ctrl->ctrl6.activityIntOnInt2En << 3) |
+              (ctrl->ctrl6.int12Polarity << 1);
+}
+uint8_t lis2hd12_set_ctrl(lis2hd12_ctrl_t *ctrl)
+{
+
+    static const uint8_t CTRL_REG1 = 0x20;
+    static const uint8_t NUM_CTRL_REGS = 6u;
+
+    ctrl_struct_to_regs(wr_buf, ctrl);
+
+    return lis2hd12_write_reg_dma(CTRL_REG1, wr_buf, NUM_CTRL_REGS);
 }
 
 uint8_t lis2hd12_enable_temp_sensor(void)
 {
     static const uint8_t ENABLE = (0x3 << 6);
     static const uint8_t TEMP_CFG_REG = 0x1F;
-    lis2hd12_write_reg(TEMP_CFG_REG, ENABLE);
-
+    return lis2hd12_write_reg(TEMP_CFG_REG, &ENABLE, 1);
 }
 uint8_t lis2hd12_disable_temp_sensor(void)
 {
     static const uint8_t DISABLE = 0x00;
     static const uint8_t TEMP_CFG_REG = 0x1F;
-    lis2hd12_write_reg(TEMP_CFG_REG, ENABLE);
-
-
+    return lis2hd12_write_reg(TEMP_CFG_REG, (uint8_t)&DISABLE, 1);
 }
 uint8_t lis2hd12_temp_sensor_is_enabled(bool *is_enabled)
 {
     static const uint8_t TEMP_CFG_REG = 0x1F;
-    lis2hd12_read_reg(TEMP_CFG_REG, is_enabled, sizeof(uint8_t));
+    
+    return lis2hd12_read_reg(TEMP_CFG_REG, (uint8_t*)is_enabled, sizeof(uint8_t));
 }
 uint8_t lis2hd12_get_interrupt_ref(uint8_t *interrupt_ref)
 {
@@ -110,11 +250,11 @@ uint8_t lis2hd12_get_interrupt_ref(uint8_t *interrupt_ref)
 
     return lis2hd12_read_reg(REF_REG, interrupt_ref, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_interrupt_ref(uint8_t *interrupt_ref)
+uint8_t lis2hd12_set_interrupt_ref(uint8_t interrupt_ref)
 {
     static const uint8_t REF_REG = 0x26;
 
-    return lis2hd12_write_reg(REF_REG, interrupt_ref);
+    return lis2hd12_write_reg(REF_REG, &interrupt_ref, 1);
 }
 uint8_t lis2hd12_get_status(uint8_t *status)
 {
@@ -126,8 +266,14 @@ uint8_t lis2hd12_get_status(uint8_t *status)
 uint8_t lis2hd12_get_acc_xyz(lis2dh12_acc_xyz *xyz)
 {
     static const uint8_t X_REG = 0x29;
+    uint8_t xyz_regs[6];
+    uint8_t ret = lis2hd12_read_reg(X_REG, xyz_regs, 3 * sizeof(uint16_t));
 
-    return lis2hd12_read_reg(X_REG, xyz, 3 * sizeof(uint16_t));
+    xyz->x = xyz_regs[1] << 8 | xyz_regs[0];
+    xyz->y = xyz_regs[3] << 8 | xyz_regs[2];
+    xyz->z = xyz_regs[5] << 8 | xyz_regs[4];
+    
+    return ret;
 }
 uint8_t lis2hd12_get_fifo_ctrl(uint8_t *fifo_ctrl)
 {
@@ -135,11 +281,11 @@ uint8_t lis2hd12_get_fifo_ctrl(uint8_t *fifo_ctrl)
 
     return lis2hd12_read_reg(FIFO_CTRL_REG, fifo_ctrl, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_fifo_ctrl(uint8_t *fifo_ctrl)
+uint8_t lis2hd12_set_fifo_ctrl(uint8_t fifo_ctrl)
 {
     static const uint8_t FIFO_CTRL_REG = 0x2E;
 
-    return lis2hd12_write_reg(FIFO_CTRL_REG, fifo_ctrl);
+    return lis2hd12_write_reg(FIFO_CTRL_REG, &fifo_ctrl, 1);
 }
 uint8_t lis2hd12_get_fifo_src(uint8_t *fifo_src)
 {
@@ -148,17 +294,17 @@ uint8_t lis2hd12_get_fifo_src(uint8_t *fifo_src)
     return lis2hd12_read_reg(FIFO_SRC_REG, fifo_src, sizeof(uint8_t));
 }
 
-uint8_t lis2hd12_get_int1_cfg(uint8_t *int1_cfg)
+uint8_t lis2hd12_get_int1_cfg(uint8_t int1_cfg)
 {
     static const uint8_t INT1_CFG_REG = 0x30;
 
-    return lis2hd12_read_reg(INT1_CFG_REG, int1_cfg, sizeof(uint8_t));
+    return lis2hd12_read_reg(INT1_CFG_REG, &int1_cfg, sizeof(uint8_t));
 }
 uint8_t lis2hd12_set_int1_cfg(uint8_t *int1_cfg)
 {
     static const uint8_t INT1_CFG_REG = 0x30;
 
-    return lis2hd12_write_reg(INT1_CFG_REG, int1_cfg);
+    return lis2hd12_write_reg(INT1_CFG_REG, int1_cfg, 1);
 }
 uint8_t lis2hd12_get_int1_src(uint8_t *int1_src)
 {
@@ -167,18 +313,17 @@ uint8_t lis2hd12_get_int1_src(uint8_t *int1_src)
     return lis2hd12_read_reg(INT1_SRC_REG, int1_src, sizeof(uint8_t));
 }
 
-
 uint8_t lis2hd12_get_int1_ths(uint8_t *int1_ths)
 {
     static const uint8_t INT1_THS_REG = 0x32;
 
     return lis2hd12_read_reg(INT1_THS_REG, int1_ths, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_int1_ths(uint8_t *int1_ths)
+uint8_t lis2hd12_set_int1_ths(uint8_t int1_ths)
 {
     static const uint8_t INT1_THS_REG = 0x32;
 
-    return lis2hd12_write_reg(INT1_THS_REG, int1_ths);
+    return lis2hd12_write_reg(INT1_THS_REG, &int1_ths, 1);
 }
 uint8_t lis2hd12_get_int1_duration(uint8_t *int1_duration)
 {
@@ -186,13 +331,12 @@ uint8_t lis2hd12_get_int1_duration(uint8_t *int1_duration)
 
     return lis2hd12_read_reg(INT1_THS_DURATION, int1_duration, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_int1_duration(uint8_t *int1_duration)
+uint8_t lis2hd12_set_int1_duration(uint8_t int1_duration)
 {
     static const uint8_t INT1_THS_DURATION = 0x33;
 
-    return lis2hd12_write_reg(INT1_THS_DURATION, int1_duration);
+    return lis2hd12_write_reg(INT1_THS_DURATION, &int1_duration, 1);
 }
-
 
 uint8_t lis2hd12_get_int2_cfg(uint8_t *int2_cfg)
 {
@@ -200,11 +344,11 @@ uint8_t lis2hd12_get_int2_cfg(uint8_t *int2_cfg)
 
     return lis2hd12_read_reg(INT2_CFG_REG, int2_cfg, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_int2_cfg(uint8_t *int2_cfg)
+uint8_t lis2hd12_set_int2_cfg(uint8_t int2_cfg)
 {
     static const uint8_t INT2_CFG_REG = 0x34;
 
-    return lis2hd12_write_reg(INT2_CFG_REG, int2_cfg);
+    return lis2hd12_write_reg(INT2_CFG_REG, &int2_cfg, 1);
 }
 uint8_t lis2hd12_get_int2_src(uint8_t *int2_src)
 {
@@ -219,11 +363,11 @@ uint8_t lis2hd12_get_int2_ths(uint8_t *int2_ths)
 
     return lis2hd12_read_reg(INT2_THS_REG, int2_ths, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_int2_ths(uint8_t *int2_ths)
+uint8_t lis2hd12_set_int2_ths(uint8_t int2_ths)
 {
     static const uint8_t INT2_THS_REG = 0x36;
 
-    return lis2hd12_write_reg(INT2_THS_REG, int2_ths);
+    return lis2hd12_write_reg(INT2_THS_REG, &int2_ths, 1);
 }
 
 uint8_t lis2hd12_get_int2_duration(uint8_t *int2_duration)
@@ -232,11 +376,11 @@ uint8_t lis2hd12_get_int2_duration(uint8_t *int2_duration)
 
     return lis2hd12_read_reg(INT2_THS_DURATION, int2_duration, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_int2_duration(uint8_t *int2_duration)
+uint8_t lis2hd12_set_int2_duration(uint8_t int2_duration)
 {
     static const uint8_t INT1_THS_DURATION = 0x37;
 
-    return lis2hd12_write_reg(INT1_THS_DURATION, int2_duration);
+    return lis2hd12_write_reg(INT1_THS_DURATION, &int2_duration, 1);
 }
 
 uint8_t lis2hd12_get_click_cfg(uint8_t *click_cfg)
@@ -245,11 +389,11 @@ uint8_t lis2hd12_get_click_cfg(uint8_t *click_cfg)
 
     return lis2hd12_read_reg(CLICK_CFG_REG, click_cfg, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_click_cfg(uint8_t *click_cfg)
+uint8_t lis2hd12_set_click_cfg(uint8_t click_cfg)
 {
     static const uint8_t CLICK_CFG_REG = 0x38;
 
-    return lis2hd12_write_reg(CLICK_CFG_REG, click_cfg);
+    return lis2hd12_write_reg(CLICK_CFG_REG, &click_cfg, 1);
 }
 
 uint8_t lis2hd12_get_click_src(uint8_t *click_src)
@@ -269,7 +413,7 @@ uint8_t lis2hd12_set_click_ths(uint8_t *click_ths)
 {
     static const uint8_t CLICK_THS_REG = 0x3A;
 
-    return lis2hd12_write_reg(CLICK_THS_REG, click_ths);
+    return lis2hd12_write_reg(CLICK_THS_REG, click_ths, 1);
 }
 uint8_t lis2hd12_get_time_limit(uint8_t *time_limit)
 {
@@ -277,11 +421,11 @@ uint8_t lis2hd12_get_time_limit(uint8_t *time_limit)
 
     return lis2hd12_read_reg(TIME_LIMIT_REG, time_limit, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_time_limit(uint8_t *time_limit)
+uint8_t lis2hd12_set_time_limit(uint8_t time_limit)
 {
     static const uint8_t TIME_LIMIT_REG = 0x3B;
 
-    return lis2hd12_write_reg(TIME_LIMIT_REG, time_limit);
+    return lis2hd12_write_reg(TIME_LIMIT_REG, &time_limit, 1);
 }
 uint8_t lis2hd12_get_time_latency(uint8_t *time_latency)
 {
@@ -289,11 +433,11 @@ uint8_t lis2hd12_get_time_latency(uint8_t *time_latency)
 
     return lis2hd12_read_reg(TIME_LATENCY_REG, time_latency, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_time_latency(uint8_t *time_latency)
+uint8_t lis2hd12_set_time_latency(uint8_t time_latency)
 {
     static const uint8_t TIME_LATENCY_REG = 0x3C;
 
-    return lis2hd12_write_reg(TIME_LATENCY_REG, time_latency);
+    return lis2hd12_write_reg(TIME_LATENCY_REG, &time_latency, 1);
 }
 uint8_t lis2hd12_get_time_window(uint8_t *time_window)
 {
@@ -301,11 +445,11 @@ uint8_t lis2hd12_get_time_window(uint8_t *time_window)
 
     return lis2hd12_read_reg(TIME_WINDOW_REG, time_window, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_time_window(uint8_t *time_window)
+uint8_t lis2hd12_set_time_window(uint8_t time_window)
 {
     static const uint8_t TIME_WINDOW_REG = 0x3D;
 
-    return lis2hd12_write_reg(TIME_WINDOW_REG, time_window);
+    return lis2hd12_write_reg(TIME_WINDOW_REG, &time_window, 1);
 }
 
 uint8_t lis2hd12_get_act_ths(uint8_t *act_ths)
@@ -314,11 +458,11 @@ uint8_t lis2hd12_get_act_ths(uint8_t *act_ths)
 
     return lis2hd12_read_reg(ACT_THS_REG, act_ths, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_act_ths(uint8_t *act_ths)
+uint8_t lis2hd12_set_act_ths(uint8_t act_ths)
 {
     static const uint8_t ACT_THS_REG = 0x3E;
 
-    return lis2hd12_write_reg(ACT_THS_REG, act_ths);
+    return lis2hd12_write_reg(ACT_THS_REG, &act_ths, 1);
 }
 uint8_t lis2hd12_get_act_dur(uint8_t *act_dur)
 {
@@ -326,9 +470,9 @@ uint8_t lis2hd12_get_act_dur(uint8_t *act_dur)
 
     return lis2hd12_read_reg(ACT_DUR_REG, act_dur, sizeof(uint8_t));
 }
-uint8_t lis2hd12_set_act_dur(uint8_t *act_dur)
+uint8_t lis2hd12_set_act_dur(uint8_t act_dur)
 {
     static const uint8_t ACT_DUR_REG = 0x3F;
 
-    return lis2hd12_write_reg(ACT_DUR_REG, act_dur);
+    return lis2hd12_write_reg(ACT_DUR_REG, &act_dur, 1);
 }
